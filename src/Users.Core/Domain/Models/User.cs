6ca0cda3bfce359654125ba890aea0511a551ffd
Vincenzo Carlino <vincenzocarlino147@gmail.com>
2022-08-20 14:46:25 +0200

    Initial Commit

---
+++ src/Users.Core/Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Users.Core.Domain.Models;
+
+using System;
+
+public class User
+{
+    public Guid Id { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string Email { get; }
+
+    public User(Guid id, string firstName, string lastName, string email)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+}
---
+++ src/Users.Core/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Users.Core.Domain.Repositories;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Users.Core.Domain.Models;
+
+internal interface IUserRepository
+{
+    Task<IEnumerable<User>> GetUsersAsync();
+}
